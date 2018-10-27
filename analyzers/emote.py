482c2f1468499c61e2898970e72b92dfe97c1b85
Jurgen <jurgen.baas@outlook.com>
2018-10-28 00:09:53 +0200

    Finished main functionality of emote analyzer.

--- analyzers/emote.py
+++ analyzers/emote.py
@@ -1,55 +1,58 @@
-"""
- Emoji/Emoticon     https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
- - Frequency per emote
- - percent of messages containing emotes and how much
- - Most popular emoji per month
-"""
-# TODO: per person or combined?
-
 import pandas as pd
 from collections import Counter
 import numpy as np
-import sys
 
 
-def most_popular(df: pd.DataFrame, top_n: int, per="month"):
-    pass
-    tdf = df[["datetime"]]
-    tdf["emotes"] = df["emoji"] + df["emoticons"]
+def most_popular_per_month(df, top_n=3):
+    """
+    :param df: A parsed and preprocessed texts DataFrame
+    :type df: pandas.DataFrame
 
+    :param top_n: The top n most popular emotes that should be returned
+    :type top_n: int
+
+    :return: A series object index on "name" and "datetime" month with tuples of the most popular emote and the
+    frequency
+    :rtype: pandas.Series
+    """
+    tdf = df[["name", "datetime"]].copy()
+    tdf.loc[:, "emotes"] = df["emoji"] + df["emoticons"]
     tdf.set_index(pd.DatetimeIndex(tdf["datetime"]), inplace=True)
-    interval_grouper = tdf.groupby(pd.Grouper(freq="M"))
-    print(tdf)
+    interval_grouper = tdf.groupby(["name", pd.Grouper(freq="M")])
+
+    emotes_per_month = interval_grouper["emotes"].sum()
+    emotes_per_month[emotes_per_month == tuple()] = np.nan
+    emotes_per_month.dropna(inplace=True)
+
+    count_per_month = emotes_per_month.apply(Counter)
+    return count_per_month.apply(lambda c: c.most_common(top_n)[0:top_n])
 
 
-def number_per_message(df: pd.DataFrame):
+def number_per_message(df):
     """
     :param df: A parsed and preprocessed texts DataFrame
-    :type: pandas.DataFrame
+    :type df: pandas.DataFrame
 
-    :return: Counter object of the number of number of emotes in a message
-    :rtype: collections.Counter
+    :return: A series object index on "name" with Counter objects of the number of emotes in a message
+    :rtype: pandas.Series
     """
-    message_grouper = df.groupby("message_id")
-    message_emotes = message_grouper["emoji", "emoticons"].agg("sum")
-    return Counter((message_emotes["emoji"] + message_emotes["emoticons"]).apply(lambda s: len(s)))
+    df["emotes"] = df["emoji"] + df["emoticons"]
+    message_grouper = df.groupby(["name", "message_id"])
+    message_emotes = message_grouper["emotes"].agg("sum")
+    num_emotes = message_emotes.apply(lambda s: len(s))
+    return num_emotes.groupby("name").apply(list).apply(Counter)
 
 
-def number_of_emotes(df: pd.DataFrame):
+def number_of_emotes(df):
     """
     :param df: A parsed and preprocessed texts DataFrame
-    :type: pandas.DataFrame
+    :type df: pandas.DataFrame
 
-    :return: Counter object with how often each emote occurs
-    :rtype: collections.Counter
+    :return: A series object indexed on "name" with Counter objects of how often each emote occurs
+    :rtype: pandas.Series
     """
-    return Counter(df["emoji"].sum() + df["emoticons"].sum())
-
-
-def foo(df: pd.DataFrame):
-    number_of_emotes(df)
-    number_per_message(df)
-    most_popular(df, 1, per="month")
+    grouped_emotes = df.groupby("name")["emoji", "emoticons"].agg("sum").apply(lambda s: sum(s, tuple()), axis=1)
+    return grouped_emotes.apply(lambda s: Counter(s))
 
 
 if __name__ == '__main__':
