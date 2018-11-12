cd0d67366049e7d0c399736f5b1623510a3e51f4
Jurgen <jurgen.baas@outlook.com>
2018-11-12 23:26:14 +0100

    Basic landing page for UI.

---
+++ web/js/main.js
@@ -0,0 +1,36 @@
+$(window).on('load', function() {
+    $(".accordion").on("click", ".accordion-header", function() {
+        $(this).toggleClass("active").next().slideToggle();
+    });
+
+    $(".howToLink").on("click", "a[id=howToLink]", function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function(){
+
+                window.location.hash = hash;
+            });
+
+            var dst = $("#howTo").parent();
+            if (!dst.hasClass("active")) {
+                dst.toggleClass("active").next().slideToggle();
+            }
+        }
+    });
+
+    $("[type=file]").on("change", function(){
+        // Name of file and placeholder
+        var file = this.files[0].name;
+        var dflt = $(this).attr("placeholder");
+
+        if($(this).val() != ""){
+            $(this).next().text(file);
+        } else {
+            $(this).next().text(dflt);
+        }
+    });
+});
